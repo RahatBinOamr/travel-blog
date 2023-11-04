6112bb0debbb68831339a9075fed8370304cc939
RahatBinOamr <108364543+RahatBinOamr@users.noreply.github.com>
2023-11-04 19:09:58 +0600

    create authentication part and completed

--- Blog/urls.py
+++ Blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('blog_details/<pk>' ,views.blog_details ,name='blog-details'),
     path('register/',views.Register,name='register'),
     path('login/',views.Login,name='login'),
+    path('logout/',views.LogOut,name='logout'),
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Blog
-from django.views import generic
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required 
 # Create your views here.
+
+
+@login_required
 def HomePage(request):
   blogs = Blog.objects.all().order_by('blog_title')
   context={
@@ -11,7 +16,7 @@ def HomePage(request):
 
 def blog_details(request,pk=None):
   blog= Blog.objects.get(id=pk)
-  blogs= Blog.objects.all()
+  blogs= Blog.objects.all().order_by('blog_title')
   context={
     'blog': blog,
     'blogs':blogs
@@ -19,8 +24,45 @@ def blog_details(request,pk=None):
   return render(request, 'blog_detail.html', context)
 
 def Register(request):
-  return render(request, 'register.html')
+  m=""
+  if request.method=="POST":
+    name=request.POST['name']
+    email=request.POST['email']
+    pass1=request.POST['password1']
+    pass2=request.POST['password2']
+    print(name,email,pass1,pass2)
+    if(pass1!=pass2):
+        m="wrong password!!!"
+    else:
+      signUp=User.objects.create_user(
+        username=name,
+        email=email,
+        password=pass1
+      )
+      signUp.save()
+      return redirect('login')
+
+  return render(request, 'register.html',{m:m})
 
 
 def Login(request):
-  return render(request, 'login.html')
+  m=""
+  if request.method=="POST":
+    name=request.POST['name']
+    password=request.POST['password']
+    loginUser=authenticate(
+      request,
+      username=name,
+      password=password
+    )
+    if loginUser is not None:
+      login(request,loginUser)
+      return redirect('/')
+    else:
+      m="something went wrong"
+  return render(request, 'login.html',{'m':m})
+
+
+def LogOut(request):
+  logout(request)
+  return redirect('login')
